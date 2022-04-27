67388bbd773a109b2846f41bba154f3fb8e13504
katrinameng <kmeng@usc.edu>
2022-04-26 17:02:37 -0700

    draftt

--- Networked justice/main.js
+++ Networked justice/main.js
@@ -10,5 +10,45 @@ $('document').ready(function () {
     $(function () {
         $('.pixel').draggable();
     });
-
+    
+     $(function () {
+        $('.text1').draggable();
+    });
+    
+     $(function () {
+        $('.text2').draggable();
+    });
+    
+ $('.lamp1').click(function () {
+        $('.text1').toggle();
+    });
+    
+     $('.lamp2').click(function () {
+        $('.text2').toggle();
+    });
+    
+      $(function () {
+        $('.text3').draggable();
+    });
+    
+      $(function () {
+        $('.text4').draggable();
+    });
+      
+     $('.pet').click(function () {
+        $('.text3').toggle();
+    });
+    
+    
+     $('.curtain').click(function () {
+        $('.text4').toggle();
+    });
+    
+     $(function () {
+        $('.text5').draggable();
+    });
+    
+    $('.van').click(function () {
+        $('.text5').toggle();
+    });
 });
